2baff5b0ca30a95baf8231fe8fd5a31b926ae090
Anjali <k.anjaliii.1011@gmail.com>
2024-12-18 22:56:27 +0530

    episode-1

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 
-const parentdiv=React.createElement('div',{id:"div1"},
-    React.createElement('div',{id:"div2"},
-        [ React.createElement('h1',{id:"head",className:"is"},'hello world!'),React.createElement('h1',{id:"head",className:"that"},'hello!')]
-    ));
     
-    const root1=ReactDOM.createRoot(document.getElementById('parent'));
-    root1.render(parentdiv);
+// const heading= React.createElement('h1',{id:'head'},"hey from anjali");
+// root.render(heading);
+
+const HeadingElement2=()=>(<h1>hello i am  function2</h1>)
+const HeadingElement=()=>(
+   <div id="container">
+   <HeadingElement2/>
+<h1>hello i am  function</h1>
+ </div>
+)
+const root=ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<HeadingElement/>);
